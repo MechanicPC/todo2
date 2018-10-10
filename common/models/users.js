4c3fe8810cf8cfc6290395eba6f458e1e15ba241
Paweł <43812265+MechanicPC@users.noreply.github.com>
2018-10-10 22:41:33 +0200

    Custom SQL simpleCount added

--- common/models/users.js
+++ common/models/users.js
@@ -2,4 +2,23 @@
 
 module.exports = function(Users) {
 
+	Users.simpleCount = function(cb) {
+		var ds = Users.dataSource;
+        var sql = "SELECT COUNT(*) FROM PUBLIC.USERS";
+		var result;
+        
+		ds.connector.execute(sql, function (err, result) {
+
+            if (err) {console.error(err);
+			}else{
+            cb(err, result);
+			}
+        });
+	};
+	
+	Users.remoteMethod('simpleCount', {
+		http: {path: '/simpleCount', verb: 'get'},
+		returns: {root: true, type: 'number'},
+		description: 'simpleCount'
+	});
 };
